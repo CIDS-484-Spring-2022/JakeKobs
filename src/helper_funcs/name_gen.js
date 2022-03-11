30b26c52314ce51fe272a20093c1ba96615b9ea4
Jacob Kobs <jkobsx4@hotmail.com>
2022-03-11 14:52:16 -0600

    Update name gen helper function

--- src/helper_funcs/name_gen.js
+++ src/helper_funcs/name_gen.js
@@ -4,11 +4,12 @@ function genName() {
     var fullName = "";
     var firstName = "";
     var lastName = "";
+    //Loop ensures we don't get a first name that's a prefix, such as "Mr.", "Mrs.", etc...
     do {
         fullName = faker.name.findName();
         firstName = fullName.split(" ")[0];
         lastName = fullName.split(" ")[1];
-    } while (fullName.length < 5);
+    } while (firstName.length < 5);
     return [firstName, lastName];
 }
 
